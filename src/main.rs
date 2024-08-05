4314cf7e4914e599c7154a1c401d285b85e46931
igor <igor.pietrzak70@gmail.com>
2024-08-05 22:29:11 +0100

    Created mathml module to create mathml files from ParsedLean structs.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use regex::Regex;
 mod examples;
 mod lean;
+mod mathml;
 mod regex_expr;
+
 fn main() {
     let pattern = regex_expr::REGEX::new().pattern;
     let re = Regex::new(&pattern).unwrap();
-   }
+    if let Some(caps) = re.captures(examples::LE_ANTISYMM) {
+      let lean = lean::LeanParsed::new(caps);
+      let html = mathml::Markdown::new(lean);
+      match html.create_file("bug") {
+        Ok(()) => println!("File created"),
+        Err(e) => println!("Error: {e}")
+      }
+    }
 
-// TODO:
-// Generalise the regex more and more.
-// split up params and (maybe) hypothesis patterns with further regex into
-// params names and their types.
+    
+}
